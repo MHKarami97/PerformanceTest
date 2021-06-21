54e07465125e51e6d37f188a387d9dd7ad135221
Mohammad Hossein Karami <mhkarami97@gmail.com>
2021-06-21 17:14:13 +0430

    add new load test

---
+++ PerformanceTest/WcfUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PerformanceTest.ServiceReference1;
+using System;
+
+namespace PerformanceTest
+{
+    [TestClass]
+    public class WcfUnitTest
+    {
+        private static Service1Client _wcfClient;
+
+        public WcfUnitTest()
+        {
+            _wcfClient = new Service1Client();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var data = _wcfClient.GetData();
+        }
+    }
+}
