10c105b190f3b217a20b50de972b1cc01666a8ef
Mohammad Hossein Karami <mhkarami97@gmail.com>
2021-06-26 10:41:02 +0430

    change grpc project

--- GRPC/Program.cs
+++ GRPC/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Runtime.Serialization;
+using System.ServiceModel;
 using System.Threading.Tasks;
 using Grpc.Core;
+using ProtoBuf.Grpc.Client;
 
 namespace GRPC
 {
@@ -8,26 +11,49 @@ namespace GRPC
     {
         static async Task Main(string[] args)
         {
-            // var channel = new Channel("localhost", 10042, ChannelCredentials.Insecure);
-            // try
-            // {
-            //     var calculator = channel.CreateGrpcService<ICalculator>();
-            //     
-            //     var response = await calculator.MultiplyAsync(new MultiplyRequest
-            //     {
-            //         X = 2,
-            //         Y = 4
-            //     });
-            //     
-            //     if (response.Result != 8)
-            //     {
-            //         throw new InvalidOperationException();
-            //     }
-            // }
-            // finally
-            // {
-            //     await channel.ShutdownAsync();
-            // }
+            var channel = new Channel("localhost", 10042, ChannelCredentials.Insecure);
+            try
+            {
+                var calculator = channel.CreateGrpcService<ICalculator>();
+                
+                var response = await calculator.MultiplyAsync(new MultiplyRequest
+                {
+                    X = 2,
+                    Y = 4
+                });
+                
+                if (response.Result != 8)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+            finally
+            {
+                await channel.ShutdownAsync();
+            }
         }
+    }
+    
+    [DataContract]
+    public class MultiplyRequest
+    {
+        [DataMember(Order = 1)]
+        public int X { get; set; }
+
+        [DataMember(Order = 2)]
+        public int Y { get; set; }
+    }
+
+    [DataContract]
+    public class MultiplyResult
+    {
+        [DataMember(Order = 1)]
+        public int Result { get; set; }
+    }
+    
+    [ServiceContract(Name = "Hyper.Calculator")]
+    public interface ICalculator
+    {
+        ValueTask<MultiplyResult> MultiplyAsync(MultiplyRequest request);
     }
 }
---
+++ GRPC/Server.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using Grpc.Core;
+using ProtoBuf.Grpc.Server;
+
+namespace GRPC
+{
+    public class ServerGrpc
+    {
+        static async Task Main2()
+        {
+            try
+            {
+                const int port = 10042;
+
+                var server = new Server
+                {
+                    Ports =
+                    {
+                        new ServerPort("localhost", port, ServerCredentials.Insecure)
+                    }
+                };
+
+                server.Services.AddCodeFirst(new MyCalculator());
+            
+                server.Start();
+
+                Console.WriteLine("server listening on port " + port);
+            
+                Console.ReadKey();
+                
+                await server.ShutdownAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                
+                throw;
+            }
+        }
+    }
+    
+    [ServiceContract(Name = "Hyper.Calculator")]
+    public interface ICalculatorServer
+    {
+        ValueTask<MultiplyResult> MultiplyAsync(MultiplyRequest request, ServerCallContext context = default);
+    }
+
+    class MyCalculator : ICalculatorServer
+    {
+        public ValueTask<MultiplyResult> MultiplyAsync(MultiplyRequest request, ServerCallContext context = default)
+        {
+            Console.WriteLine($"Processing request from {context.Peer}");
+            
+            var result = request.X * request.Y;
+
+            return new ValueTask<MultiplyResult>();
+        }
+    }
+}
