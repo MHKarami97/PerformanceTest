372e098aa4a539cd39d9da4b5d3a10204a869532
Mohammad Hossein Karami <mhkarami97@gmail.com>
2021-06-26 15:17:19 +0430

    change grpc project

--- GRPC/Program.cs
+++ GRPC/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Runtime.Serialization;
-using System.ServiceModel;
 using System.Threading.Tasks;
 using Grpc.Core;
 using ProtoBuf.Grpc.Client;
@@ -15,45 +13,24 @@ namespace GRPC
             try
             {
                 var calculator = channel.CreateGrpcService<ICalculator>();
-                
+
                 var response = await calculator.MultiplyAsync(new MultiplyRequest
                 {
                     X = 2,
                     Y = 4
                 });
-                
+
                 if (response.Result != 8)
                 {
                     throw new InvalidOperationException();
                 }
+
+                Console.WriteLine(response.Result);
             }
             finally
             {
                 await channel.ShutdownAsync();
             }
         }
-    }
-    
-    [DataContract]
-    public class MultiplyRequest
-    {
-        [DataMember(Order = 1)]
-        public int X { get; set; }
-
-        [DataMember(Order = 2)]
-        public int Y { get; set; }
-    }
-
-    [DataContract]
-    public class MultiplyResult
-    {
-        [DataMember(Order = 1)]
-        public int Result { get; set; }
-    }
-    
-    [ServiceContract(Name = "Hyper.Calculator")]
-    public interface ICalculator
-    {
-        ValueTask<MultiplyResult> MultiplyAsync(MultiplyRequest request);
     }
 }
