73d5fa11331b1b28157c5afbdb0c480edd2f57ed
Mohammad Hossein Karami <mhkarami97@gmail.com>
2021-07-04 11:53:14 +0430

    update jmeter test, update grpc project

--- GRPcService/Program.cs
+++ GRPcService/Program.cs
@@ -3,6 +3,8 @@ using ProtoBuf.Grpc.Server;
 using System;
 using System.Threading.Tasks;
 using GRPC;
+using ProtoBuf.Grpc.Reflection;
+using ProtoBuf.Meta;
 
 namespace GRPcService
 {
@@ -18,13 +20,25 @@ namespace GRPcService
                 {
                     Ports =
                     {
-                        new ServerPort("Krj-karami", port, ServerCredentials.Insecure)
+                        new ServerPort("localhost", port, ServerCredentials.Insecure)
                     }
                 };
 
                 server.Services.AddCodeFirst(new MyCalculator());
 
                 server.Start();
+
+                var generator = new SchemaGenerator
+                {
+                    ProtoSyntax = ProtoSyntax.Proto3
+                };
+
+                var schema = generator.GetSchema<ICalculator>(); // there is also a non-generic overload that takes Type
+
+                using (var writer = new System.IO.StreamWriter("services.proto"))
+                {
+                    await writer.WriteAsync(schema);
+                }
 
                 Console.WriteLine("server listening on port " + port);
 
