6214e2e598c0223b2efc57f6ada53641eddfb673
Mohammad Hossein Karami <mhkarami97@gmail.com>
2021-07-11 11:22:56 +0430

    add console test

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ConsoleTest
+{
+    class Program
+    {
+        private const int MaxWcfCall = 500;
+
+        static void Main(string[] args)
+        {
+            CallWcf(true);
+        }
+
+        private static void CallWcf(bool withSingleInstance)
+        {
+            if (withSingleInstance)
+            {
+                CallWcfSingleInstance();
+            }
+            else
+            {
+                CallWcfMultiInstance();
+            }
+        }
+
+        private static void CallWcfSingleInstance()
+        {
+            for (var i = 0; i < MaxWcfCall; i++)
+            {
+                WcfCall.Instance().CallWcf();
+            }
+        }
+
+        private static void CallWcfMultiInstance()
+        {
+            for (var i = 0; i < MaxWcfCall; i++)
+            {
+                WcfCall.NewInstance().CallWcf();
+            }
+        }
+    }
+}
---
+++ ConsoleTest/WcfCall.cs
@@ -0,0 +1,42 @@
+﻿using ConsoleTest.ServiceReference;
+
+namespace ConsoleTest
+{
+    public class WcfCall
+    {
+        private static WcfCall _instance;
+        private static Service1Client _service;
+        private static readonly object Lock = new object();
+
+        private WcfCall()
+        {
+            _service = new Service1Client();
+        }
+
+        public static WcfCall Instance()
+        {
+            if (_instance == null)
+            {
+                lock (Lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new WcfCall();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+        public static WcfCall NewInstance()
+        {
+            return new WcfCall();
+        }
+
+        public void CallWcf()
+        {
+            var result = _service.GetData();
+        }
+    }
+}
